9b0bec973c62cdc39d6f61d3e039c19f7e8bc648
Ilia Lesik <ilialesik@gmail.com>
2020-02-09 13:51:02 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+
+module.exports = (env, argv) => ({
+    mode: argv.mode === 'production' ? 'production' : 'development',
+
+    // This is necessary because Figma's 'eval' works differently than normal eval
+    devtool: argv.mode === 'production' ? false : 'inline-source-map',
+
+    entry: {
+        code: './src/code.ts', // The entry point for your UI code
+    },
+
+    module: {
+        rules: [
+            // Converts TypeScript code to JavaScript
+            { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
+        ],
+    },
+
+    // Webpack tries these extensions for you if you omit the extension like "import './file'"
+    resolve: { extensions: ['.tsx', '.ts', '.jsx', '.js'] },
+
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'), // Compile into a folder called "dist"
+    },
+
+    // Tells Webpack to generate "ui.html" and to inline "ui.ts" into it
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/ui.html',
+            filename: 'ui.html',
+            inlineSource: '.(js)$',
+            chunks: ['ui'],
+        }),
+        new HtmlWebpackInlineSourcePlugin(),
+    ],
+});
